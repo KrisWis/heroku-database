4ca06b57ba2cd59dd503b4c1db74829100c9772e
Evgeniy <nekrasov.2h@yandex.ru>
2022-02-22 18:52:07 +0300

    s

--- tgbot.py
+++ tgbot.py
@@ -44,7 +44,7 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 driver = selenium.webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 
-def gdz_API(result,message):
+def gdz_API(result):
 
     form = login_html.select('form')[0]
     form.select('input')[0]['value'] = result
@@ -115,7 +115,7 @@ def callback_worker(call):
     if call.data == "yes":
         bot.send_message(call.message.chat.id, 'Начинаю поиск... '
                                                '\nПоиск завершится примерно через 9 секунд')
-        gdz_API(result,call.message)
+        gdz_API(result, call.message)
         photo = open('../venv/gdz_image.jpg', 'rb')
         bot.send_photo(call.message.chat.id, photo, 'Это то, что ты искал?')
         bot.register_next_step_handler(call.message,recheck)
@@ -134,7 +134,7 @@ def recheck(message):
         rand_phrase = random.choice(['Хм... поищу ещё', "Пойду искать дальше...", 'Поищу поглубже', "Подключаю свои лучшие навыки", "Продолжаю искать...", "Продолжаю поиск..."])
         bot.send_message(message.from_user.id, rand_phrase)
         num += 4
-        gdz_API(result,message)
+        gdz_API(result, message)
         photo = open('../venv/gdz_image.jpg', 'rb')
         rand_phrase2 = random.choice(['Может быть это?', "Хм.. Может это?", 'Это то, что надо?', "May be это?", "Как насчёт этого?", "Это подойдёт?"])
         bot.send_photo(message.chat.id, photo, rand_phrase2)
