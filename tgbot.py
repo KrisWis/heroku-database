46bf4a2ca3804c690796d5c02b07a03816b6e16f
Evgeniy <nekrasov.2h@yandex.ru>
2022-02-24 19:47:53 +0300

    s

--- tgbot.py
+++ tgbot.py
@@ -14,12 +14,7 @@ import psycopg2
 
 
 bot = telebot.TeleBot(BOT_TOKEN)
-server = Flask(__name__)
-logger = telebot.logger
-logger.setLevel(logging.DEBUG)
 
-db_connection = psycopg2.connect(DB_URI, sslmode="require")
-db_object = db_connection.cursor()
 
 
 name_subject = ''
@@ -76,13 +71,7 @@ def gdz_API(result):
 
 def start(message):
     global stop
-    id = message.from_user.id
-    db_object.execute(f"SELECT id FROM users WHERE id = {user_id}")
-    result = db_object.fetchone()
 
-    if not result:
-        db_object.execute("INSERT INTO users(id, username, messages) VALUES (%s, %s, %s)", (user_id, username, 0))
-        db_connection.commit()
 
     if message.text == '/start':
         bot.send_message(message.from_user.id, 'Привет! Я бот, который поможет тебе с учёбой! \nТебе всего лишь надо ввести название учебника, его автора и номер, который нужно решить. '
@@ -146,15 +135,4 @@ def recheck(message):
         bot.send_photo(message.chat.id, photo, rand_phrase2)
         bot.register_next_step_handler(message,recheck)
 
-@server.route(f"/{BOT_TOKEN}", methods=["POST"])
-def redirect_message():
-    json_string = request.get_data().decode("utf-8")
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
-
-
-if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+bot.polling(none_stop=True, interval=0)
