6617d268972ca448ae6f8e7b4610528213c5683b
Evgeniy <nekrasov.2h@yandex.ru>
2022-02-25 22:25:22 +0300

    s

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = '5031716672:AAGsyJ0wMMS3uq662Yxj3pY6Vg8boJ8oHfw'
-APP_URL = "https://gdzbottelegram.herokuapp.com/" + BOT_TOKEN
-DB_URI = "postgres://elavqerzcmfsaj:2f8e50db6d4ee15f596186260ec6b01e44e4b5e8fcaa7235f4e44da2c8390871@ec2-44-197-94-126.compute-1.amazonaws.com:5432/d7vrgtvvqe96ec"
+APP_URL = "https://botgdztg.herokuapp.com/" + BOT_TOKEN
+DB_URI = "postgres://pekpduusssxzcx:fa21a9e8b69bf36a1b702d3d7520a86d7a8dcef8f07e6e8cdb98bd98b840209c@ec2-52-208-221-89.eu-west-1.compute.amazonaws.com:5432/dblvro9h4ink5u"
