71b4bb68aeaad12bc84f5508add8c3abb0dad432
Evgeniy <nekrasov.2h@yandex.ru>
2022-02-26 20:58:40 +0300

    s

--- tgbot.py
+++ tgbot.py
@@ -64,12 +64,7 @@ def gdz_API(result):
     item = elem.find_element(By.TAG_NAME, 'img')
     url = item.get_attribute('src')
 
-    db_object.execute(f"SELECT user_result FROM users WHERE user_result = {url}")
-    result2 = db_object.fetchone()
 
-    if not result2:
-        db_object.execute("INSERT INTO users(user_result) VALUES (%s)", (url))
-        db_connection.commit()
 
 
     img_data = requests.get(url).content
