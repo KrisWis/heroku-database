6f299cc3a05e87dbd10d880b96375c8e87316c74
Evgeniy <nekrasov.2h@yandex.ru>
2022-02-28 21:27:17 +0300

    s

--- tgbot.py
+++ tgbot.py
@@ -73,13 +73,7 @@ def gdz_API(result):
 
 def start(message):
     global stop
-    user_id = message.from_user.id
-    db_object.execute(f"SELECT user_request FROM users WHERE user_request = {user_id}")
-    result3 = db_object.fetchone()
 
-    if not result3:
-        db_object.execute("INSERT INTO users(user_result, user_request) VALUES (%s, %s)", (0, user_id))
-        db_connection.commit()
 
     if message.text == '/start':
 
@@ -108,7 +102,12 @@ def get_result_func(message):
 
 
     result = message.text
+    db_object.execute(f"SELECT user_request FROM users WHERE user_request = {result}")
+    result2 = db_object.fetchone()
 
+    if not result2:
+        db_object.execute("INSERT INTO users(user_result, user_request) VALUES (%s, %s)", (0, result))
+        db_connection.commit()
 
     if len(result) >= 10:
         answer = 'Ты хочешь найти ГДЗ по запросу "{}"?'.format(result)
