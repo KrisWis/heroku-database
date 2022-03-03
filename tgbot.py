a00c65391c350f26ef6af5659c1ce1bb726e1809
Evgeniy <nekrasov.2h@yandex.ru>
2022-03-03 22:34:25 +0300

    s

--- tgbot.py
+++ tgbot.py
@@ -59,6 +59,7 @@ def gdz_API(result):
     elem = driver.find_element(By.CLASS_NAME, 'with-overtask')
     item = elem.find_element(By.TAG_NAME, 'img')
     url = item.get_attribute('src')
+    url = str(url)
     db_object.execute(f"SELECT user_result FROM users WHERE user_result = {url}")
     result3 = db_object.fetchone()
 
