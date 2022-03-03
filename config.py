e9ce1e387bd96a800201fc5dfc943237d22839d5
Evgeniy <nekrasov.2h@yandex.ru>
2022-03-03 12:19:39 +0300

    s

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = '5031716672:AAGsyJ0wMMS3uq662Yxj3pY6Vg8boJ8oHfw'
 APP_URL = "https://botgdztg.herokuapp.com/" + BOT_TOKEN
-DB_URI = "postgres://pekpduusssxzcx:fa21a9e8b69bf36a1b702d3d7520a86d7a8dcef8f07e6e8cdb98bd98b840209c@ec2-52-208-221-89.eu-west-1.compute.amazonaws.com:5432/dblvro9h4ink5u"
+DB_URI = "postgres://sdanxrdnuzgkkw:b70ee0e1ca2bab1b71475b4acdf0c3d93950b351f52cea49be6bb565d9a141e9@ec2-54-194-147-61.eu-west-1.compute.amazonaws.com:5432/db0k7977e2roua"
