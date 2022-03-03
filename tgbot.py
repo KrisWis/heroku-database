aa868ace5c64b3f12241f5ceeac7dd6e6d7c0230
Evgeniy <nekrasov.2h@yandex.ru>
2022-03-03 12:38:11 +0300

    s

--- tgbot.py
+++ tgbot.py
@@ -76,14 +76,7 @@ def start(message):
 
 
     if message.text == '/start':
-        user_id = message.from_user.id
 
-        db_object.execute(f"SELECT user_result FROM users WHERE id = {user_id}")
-        result2 = db_object.fetchone()
-
-        if not result2:
-            db_object.execute("INSERT INTO users(user_result) VALUES (%s)", user_id)
-            db_connection.commit()
 
         bot.send_message(message.from_user.id, 'Привет! Я бот, который поможет тебе с учёбой! \nТебе всего лишь надо ввести название учебника, его автора и номер, который нужно решить. '
                                                'Попробуй!')
